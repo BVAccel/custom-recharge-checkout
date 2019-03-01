bae3873b176eb027026f0e8deac91307e5b491a3
Kyle Villeneuve <kylevilleneuve@gmail.com>
2019-02-28 16:36:23 -0800

    more stuff

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -4,6 +4,7 @@ const ts = require("gulp-typescript");
 const uglify = require("gulp-uglify");
 const htmlmin = require("gulp-htmlmin");
 const imagemin = require("gulp-imagemin");
+const clean = require('gulp-clean');
 
 gulp.task("scss", () => {
   return gulp
@@ -14,17 +15,24 @@ gulp.task("scss", () => {
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("ts", function() {
-  return gulp
-    .src("./src/ts/**.ts")
-    .pipe(tsProject())
-    .pipe(uglify())
-    .pipe(gulp.dest("../server/dist/public"));
+gulp.task("clean", () => {
+  return gulp.src('../server/dist')
+  .pipe(clean({ read: false, force: true }))
+})
+
+gulp.task("ts", () => {
+  return (
+    gulp
+      .src("./src/ts/**.ts")
+      .pipe(tsProject().on("error", console.log))
+      .pipe(uglify())
+      .pipe(gulp.dest("../server/dist/public"))
+  );
 });
 
 gulp.task("html", () => {
   return gulp
-    .src("./src/*.html")
+    .src("./src/views/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("../server/dist/public"));
 });
@@ -36,11 +44,13 @@ gulp.task("img", () => {
     .pipe(gulp.dest("../server/dist/public/img"));
 });
 
-gulp.task("watch", () => {
-  gulp.watch("./src/scss/**.scss", ["scss"]);
-  gulp.watch("./src/ts/**.ts", ["ts"]);
-  gulp.watch("./src/**.html", ["html"]);
-  gulp.watch("./src/img/**", ["img"]);
-});
 
-gulp.task("default", ["ts", "scss", "html", "img"]);
+gulp.task("default", gulp.series(["clean", "ts", "scss", "html", "img"]));
+
+
+gulp.task("watch", gulp.series(['default'], () => {
+  gulp.watch("./src/scss/**.scss", gulp.series(["scss"]));
+  gulp.watch("./src/ts/**.ts", gulp.series(["ts"]));
+  gulp.watch("./src/views/**", gulp.series(["html"]));
+  gulp.watch("./src/img/**", gulp.series(["img"]));
+}));
--- client/src/ts/main.ts
+++
@@ -1,5 +0,0 @@
-(() => {
-  // 1. get checkout token
-  // 2. render checkout data + form
-  // 3. add event listener to form that submits checkout
-})();
